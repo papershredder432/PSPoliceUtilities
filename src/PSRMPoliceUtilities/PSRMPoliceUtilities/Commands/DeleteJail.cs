a3e579952edb915b0dbb07919d40693a4d4e5879
papershredder432 <34615274+papershredder432@users.noreply.github.com>
2020-12-17 19:50:45 -0500

    updated/added deletejail

--- src/PSRMPoliceUtilities/PSRMPoliceUtilities/Commands/DeleteJail.cs
+++ src/PSRMPoliceUtilities/PSRMPoliceUtilities/Commands/DeleteJail.cs
@@ -1,7 +1,44 @@
-﻿namespace PSRMPoliceUtilities.Commands
+﻿using System.Collections.Generic;
+using System.Linq;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace PSRMPoliceUtilities.Commands
 {
-    public class DeleteJail
+    public class DeleteJail : IRocketCommand
     {
-        
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer unturnedPlayer = (UnturnedPlayer) caller;
+            
+            var jails = PSRMPoliceUtilities.Instance.Configuration.Instance.Jails;
+            var findJail = jails.FirstOrDefault(x => x.Name.ToLower() == command[0].ToLower());
+
+            if (command[0].Length < 1)
+            {
+                ChatManager.serverSendMessage($"Incorrect usage of command.", Color.red, null, unturnedPlayer.SteamPlayer(), EChatMode.SAY, null, true);
+                return;
+            }
+            
+            if (findJail == null)
+            {
+                ChatManager.serverSendMessage($"Jail doesn't exists", Color.red, null, unturnedPlayer.SteamPlayer(), EChatMode.SAY, null, true);
+                return;
+            }
+
+            PSRMPoliceUtilities.Instance.Configuration.Instance.Jails.Remove(findJail);
+
+            PSRMPoliceUtilities.Instance.Configuration.Save();
+            ChatManager.serverSendMessage($"Deleted jail {command[0]}", Color.green, null, unturnedPlayer.SteamPlayer(), EChatMode.SAY, null, true);
+        }
+
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "deletejail";
+        public string Help => "Deletes a jail.";
+        public string Syntax => "/deletejail <name>";
+        public List<string> Aliases=> new List<string>();
+        public List<string> Permissions => new List<string> { "ps.policeutilities.jailmanager" };
     }
 }
